3885d8df73e0cac8e74f857b1c9f9110634c6588
kumarapon933-del <kumarapon933@gmail.com>
2026-08-27 22:43:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ document.addEventListener("DOMContentLoaded", () => {
   applyBtn.innerText = SITE_CONFIG.applyButtonText;
   applyBtn.href = SITE_CONFIG.landingPage;
 });
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("page-title").innerText = SITE_CONFIG.siteTitle;
+  document.getElementById("site-title-text").innerText = SITE_CONFIG.siteTitle;
+  document.getElementById("main-header").style.backgroundColor = SITE_CONFIG.headerBgColor;
+  
+  document.getElementById("job-img").src = SITE_CONFIG.jobImage;
+  document.getElementById("job-title").innerText = SITE_CONFIG.jobTitle;
+  document.getElementById("company-name").innerText = SITE_CONFIG.companyName;
+  document.getElementById("job-desc").innerText = SITE_CONFIG.jobDescription;
+  
+  const applyBtn = document.getElementById("apply-btn");
+  applyBtn.innerText = SITE_CONFIG.applyButtonText;
+  applyBtn.href = SITE_CONFIG.landingPage;
+
+  
+  const signInBtn = document.getElementById("signin-btn");
+  if (signInBtn) {
+    signInBtn.href = SITE_CONFIG.signInUrl;
+  }
+});
