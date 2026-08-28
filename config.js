7ad97c9b5e034874d7d3f2c89d138da763135470
kumarapon933-del <kumarapon933@gmail.com>
2026-08-27 21:54:35 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const SITE_CONFIG = {
+  siteTitle: "Latest Job Opportunities",
+  landingPage: "https://surl.li/semlzw",
+  applyButtonText: "Apply Now – Free",
+  headerBgColor: "#0056b3",
+  
+  
+  jobTitle: "Senior Software Engineer",
+  companyName: "Tech Solutions Ltd.",
+  jobDescription: "We are looking for experienced developers. Click the button below to apply.",
+  jobImage: "https://via.placeholder.com/600x300"
+};
